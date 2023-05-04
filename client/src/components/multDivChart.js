431b61179b3c47fb15352f029c866cfe9212b9a3
Matthew Fanning <matt.fanning@gmail.com>
2023-05-04 00:37:34 -0300

    Added tooltip to multdiv table

--- client/src/components/multDivChart.js
+++ client/src/components/multDivChart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export default function MultDivChart() {
+   //states
    const [dimensions, setDimensions] = useState({
       rows: 10,
       cols: 10,
@@ -8,6 +9,15 @@ export default function MultDivChart() {
 
    const [cellData, setCellData] = useState([[1]]);
 
+   const [tooltipData, setTooltipData] = useState({
+      int1: "",
+      int2: "",
+      top: "",
+      left: ""
+   });
+
+
+   //when dimensions are updated, call this to fill in cellData.
    useEffect(() => {
       function fillCellData() {
          const data = [];
@@ -23,22 +33,22 @@ export default function MultDivChart() {
       return;
    },[dimensions.cols, dimensions.rows]);
 
-   
 
+   //subcomponents
    function tableHeaderRow() {
       return (
-         <tr key="header_row">
+         <tr>
             <th className="multdiv_th">0</th>
-            {tableHeaderCells()}
+            {_tableHeaderCells()}
          </tr>
       );
    };
 
-   function tableHeaderCells() {
+   function _tableHeaderCells() {
       // iterate through all cells of row 0
       return cellData[0].map((cell, index) => {
          return (
-            <th className="multdiv_th" data-col={index +1}>{cell}</th>
+            <th key={cell} className="multdiv_th" data-col={index +1}>{cell}</th>
          );
       });
    };
@@ -46,26 +56,71 @@ export default function MultDivChart() {
    function tableBody() {
       return cellData.map(row => {
          return (
-            <tr>
+            <tr key={row[0]}>
                <th className="multdiv_th" data-row={row[0]}>{row[0]}</th>
-               {tableBodyTDs(row)}
+               {_tableBodyTDs(row)}
             </tr>
          );
       });
    };
 
-   function tableBodyTDs(row) {
+   function _tableBodyTDs(row) {
       return row.map((cell, index) => {
          return (
-            <td className="multdiv_td" data-row={row[0]} data-col={index +1}
+            <td key={cell} className="multdiv_td" data-row={row[0]} data-col={index +1}
             onMouseOver={highlightHover} onMouseLeave={clearHighlightHover}>{cell}</td>
          );
       });
    };
 
+   function hoverTooltip() {
+      const {int1, int2, top, left} = tooltipData;
+
+      if(int1 == "" || int2 == "") {
+         return("");  //don't show the hover div at all.
+      } else {
+         const result = int1 * int2;
+         return(
+            <div className="multdiv_tooltip multdiv_display_flex" style={{'top':top, 'left': left}}>
+               {mathEquations(int1,int2,result)}
+            </div>
+         );
+      };
+
+   };
+
+   function mathEquations(int1, int2, result) {
+      if(int1 == int2) {
+         return (
+            <div>
+               <b className="multdiv_tooltip_text">{int1} x {int2} = {result} </b>
+               <hr className="multdiv_tooltip_line_bold"></hr>
+               <b className="multdiv_tooltip_text">{result} &#x00F7; {int1} = {int2} </b>
+            </div>
+         );
+      } else {
+         return(
+            <div>
+               <b className="multdiv_tooltip_text">{int1} x {int2} = {result} </b>
+               <hr className="multdiv_tooltip_line"></hr>
+               <b className="multdiv_tooltip_text">{int2} x {int1} = {result} </b>
+               <hr className="multdiv_tooltip_line_bold"></hr>
+               <b className="multdiv_tooltip_text">{result} &#x00F7; {int1} = {int2} </b>
+               <hr className="multdiv_tooltip_line"></hr>
+               <b className="multdiv_tooltip_text">{result} &#x00F7; {int2} = {int1} </b>
+            </div>
+         );
+      };
+      
+   }
+
+
+   //Mouse Event Handlers
    function highlightHover(event) {
       const target = event.target;
+      target.classList.add("multdiv_highlighted_target");
       
+      //table cell highlighting
       const colId = target.getAttribute('data-col');
       let affectedCells = document.querySelectorAll(`[data-col='${colId}']`);
       affectedCells.forEach(cell => {
@@ -78,20 +133,37 @@ export default function MultDivChart() {
       const rowId = event.target.getAttribute('data-row');
       affectedCells = document.querySelectorAll(`[data-row='${rowId}']`);
       affectedCells.forEach(cell => {
-         const cellDR = parseInt(cell.getAttribute('data-col')) || 0;
-         const targetDR = parseInt(target.getAttribute('data-col')) || 0;    
-         if(cellDR <= targetDR) {
+         const cellDC = parseInt(cell.getAttribute('data-col')) || 0;
+         const targetDC = parseInt(target.getAttribute('data-col')) || 0;    
+         if(cellDC <= targetDC) {
             cell.classList.add("multdiv_highlighted");
          }
       });
-   }
+
+      //tooltip position
+      const cellWidth = getComputedStyle(target).getPropertyValue('--cellWidth');
+      const xPos = target.offsetLeft + parseInt(cellWidth);
+      const yPos = target.offsetTop + 2.5 * parseInt(cellWidth) + 15;
+      
+      //tooltip content
+      const data_row = target.getAttribute('data-row');
+      const data_col = target.getAttribute('data-col');
+      
+      setTooltipData({int1: data_col, int2: data_row, top: yPos+'px', left: xPos+'px'});
+      
+   };
 
    function clearHighlightHover(event) {
+      event.target.classList.remove("multdiv_highlighted_target");
       const affectedCells = document.querySelectorAll('.multdiv_highlighted');
       affectedCells.forEach(cell => cell.classList.remove("multdiv_highlighted"));
       
-   }
+      setTooltipData({int1: "", int2: "", top: "", left: ""});
 
+   };
+
+   
+   //component's body
    return (
       <div className="multdiv_div">
          <ul className="multdiv_ul">
@@ -100,7 +172,7 @@ export default function MultDivChart() {
             </li>
             <li className="multdiv_li">
                <input type="number" className="multdiv_input" name="X" 
-                  min="1" max="20" value={dimensions.cols} onChange={event => 
+                  min="1" max="20" value={dimensions.cols} onKeyDown={()=>{return false}} onChange={event => 
                      setDimensions({rows: dimensions.rows, cols: parseInt(event.target.value)})
                   }>
                </input>
@@ -110,7 +182,7 @@ export default function MultDivChart() {
             </li>
             <li className="multdiv_li">
                <input type="number" className="multdiv_input" name="Y" 
-                  min="1" max="20" value={dimensions.rows} onChange={event =>
+                  min="1" max="20" value={dimensions.rows} onKeyDown={()=>{return false}} onChange={event =>
                      setDimensions({rows:  parseInt(event.target.value), cols: dimensions.cols})
                   }>
                </input>
@@ -124,6 +196,7 @@ export default function MultDivChart() {
                {tableBody()}
             </tbody>
          </table>
+         {hoverTooltip()}
       </div>
    );
-}
+};
